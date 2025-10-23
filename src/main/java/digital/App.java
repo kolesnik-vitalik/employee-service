2f33b7758198996bbd085d6d22fd0fb46d96b618
Kolesnik <kolesnik.vitalik21@gmail.com>
2025-10-23 14:34:54 +0300

    init project

---
+++ src/main/java/digital/App.java
@@ -0,0 +1,10 @@
+package digital;
+
+import digital.domain.Employee;
+
+public class App
+{
+    public static void main( String[] args ){
+        System.out.println(new Employee());
+    }
+}
---
+++ src/main/java/digital/domain/Employee.java
@@ -0,0 +1,15 @@
+package digital.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+}
